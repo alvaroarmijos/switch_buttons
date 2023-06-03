1525ee9b3218dcb3ed5707efe7e8046f5aab7b3c
Alvaro Armijos <alvaro.armijoss@gmail.com>
2023-06-03 00:12:56 -0500

    Create Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_buttons/pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
