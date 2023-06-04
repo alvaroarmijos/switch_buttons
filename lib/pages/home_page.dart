8ca59e994c27283d80094b9cb43fd7108befa026
Alvaro Armijos <alvaro.armijoss@gmail.com>
2023-06-03 20:41:10 -0500

    Impement all switch buttons on HomePage.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:toggle_buttons/widgets/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late bool dayNightToggle;
+  late bool lightDarkToggle;
+  late bool dayNightToggle2;
+  late bool minimalToggle;
+
+  @override
+  void initState() {
+    dayNightToggle = true;
+    lightDarkToggle = true;
+    dayNightToggle2 = true;
+    minimalToggle = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Toggle Buttons"),
+        title: const Text("Switch Buttons"),
       ),
-      body: const Column(
+      body: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          DayNightToggle(),
+          Expanded(
+            child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                color: dayNightToggle ? Colors.white : Colors.blueGrey,
+                child: Center(
+                  child: CustomSwitch(
+                    value: dayNightToggle,
+                    activeText: "Day",
+                    inactiveText: "Night",
+                    onChanged: (value) {
+                      setState(() {
+                        dayNightToggle = value;
+                      });
+                    },
+                  ),
+                )),
+          ),
+          Expanded(
+            child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                color: lightDarkToggle ? Colors.blue[50] : Colors.blueGrey[800],
+                child: Center(
+                  child: LightDarkSwitch(
+                    value: lightDarkToggle,
+                    onChanged: (value) {
+                      setState(() {
+                        lightDarkToggle = value;
+                      });
+                    },
+                  ),
+                )),
+          ),
+          Expanded(
+            child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                color: dayNightToggle2 ? Colors.white : Colors.black,
+                child: Center(
+                  child: DayNightSwitch(
+                    value: dayNightToggle2,
+                    onChanged: (value) {
+                      setState(() {
+                        dayNightToggle2 = value;
+                      });
+                    },
+                  ),
+                )),
+          ),
+          Expanded(
+            child: Container(
+                color: const Color(0xFFf3e5d3),
+                child: Center(
+                  child: MinimalSwitch(
+                    value: minimalToggle,
+                    onChanged: (value) {
+                      setState(() {
+                        minimalToggle = value;
+                      });
+                    },
+                  ),
+                )),
+          ),
         ],
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,6 @@
 library widgets;
 
-export 'day_night_toggle.dart';
+export 'custom_switch.dart';
+export 'day_night_switch.dart';
+export 'light_dark_switch.dart';
+export 'minimal_switch.dart';
